01c3e7c7fae4780d100ca2429e757289fa44afe2
Jacob Brady <polyfish42@gmail.com>
2018-06-07 13:35:10 -0400

    Youtube player embeds with correct url

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (attribute, src)
 import Html.Events exposing (onClick, onInput)
 import Http exposing (get, send)
 import Json.Decode exposing (Decoder, field, float, list, map2, string)
+import Regex exposing (find, regex)
 
 
 -- MAIN
@@ -47,20 +49,43 @@ init =
 
 
 -- VIEW
+-- <iframe width="560" height="315" src="https://www.youtube.com/embed/7Tx4PXDW35g" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ p [] [text model.errorMessage]
+        [ p [] [ text model.errorMessage ]
+        , viewVideo model.uri
         , input [ onInput UpdateUri ] []
         , button [ onClick FetchCaptions ] [ text <| "Submit" ]
         , div [] <| viewCaptions model.captions
         ]
 
+
+viewVideo : String -> Html Msg
+viewVideo uri =
+    let
+        videoCode =
+            find (Regex.AtMost 1) (regex "\\?v=(.+)$") uri
+                |> List.map .submatches
+    in
+    case videoCode of
+        [ [ Just code ] ] ->
+            viewIframe code
+
+        _ ->
+           p [] [ text <| "video not found" ]
+
+
+viewIframe : String -> Html Msg
+viewIframe videoCode =
+    iframe [ src ("https://www.youtube.com/embed/" ++ videoCode), attribute "frameborder" "0" ] []
+
+
 viewCaptions : List Caption -> List (Html Msg)
 viewCaptions captions =
-  List.map (\caption -> p [] [text <| toString caption.time ++ ": " ++ caption.text]) captions 
+    List.map (\caption -> p [] [ text <| toString caption.time ++ ": " ++ caption.text ]) captions
 
 
 
@@ -103,7 +128,7 @@ errorMessage message =
             msg
 
         Http.NetworkError ->
-          "We weren't able to fetch the captions for this video. Please check your internet connection."
+            "We weren't able to fetch the captions for this video. Please check your internet connection."
 
         Http.BadStatus res ->
             res.body
