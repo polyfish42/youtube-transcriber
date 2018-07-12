767dcca7d35c7538666f33109537c5bbfcabc5fe
Jacob Brady <polyfish42@gmail.com>
2018-07-12 17:03:18 -0400

    Add search input and filter results based on input

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -67,7 +67,7 @@ type DropDownStatus
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" "apple" Nothing Nothing Closed, Cmd.none )
+    ( Model "" "" Nothing Nothing Closed, Cmd.none )
 
 
 
@@ -81,6 +81,7 @@ type Msg
     | ToggleDropDown
     | BlurDropDown
     | CaptionPicked Caption
+    | UpdateSearch String
     | SkipToTime Float
 
 
@@ -142,6 +143,9 @@ update message model =
                         |> Maybe.andThen setCurrent
             in
             ( { model | caption = newCaptions, dropDownStatus = Closed }, Cmd.none )
+
+        UpdateSearch search ->
+            ( { model | search = String.toLower search }, Cmd.none )
 
         SkipToTime time ->
             ( model, skipToTime time )
@@ -273,6 +277,7 @@ view model =
         , input [ onInput UpdateUri ] []
         , button [ onClick FetchCaptions ] [ text <| "Submit" ]
         , viewCaptionPicker model
+        , viewSearchCaptions model.caption
         , div [ class "transcript" ] (viewCaption model.caption model.search)
         ]
 
@@ -326,51 +331,115 @@ onClick message =
         (succeed message)
 
 
+viewSearchCaptions : Maybe CaptionHistory -> Html Msg
+viewSearchCaptions history =
+    case history of
+        Just h ->
+            input [ onInput UpdateSearch ] []
+
+        Nothing ->
+            div [] []
+
+
 viewCaption : Maybe CaptionHistory -> String -> List (Html Msg)
 viewCaption captionHistory search =
     case captionHistory of
         Just history ->
-            filterBySearch history search
-                |> viewCaptions
+            viewCaptions history search
 
         Nothing ->
             [ p [] [ text "No captions found for this video" ] ]
 
 
-filterBySearch : CaptionHistory -> String -> List Line
-filterBySearch history search =
+viewCaptions : CaptionHistory -> String -> List (Html Msg)
+viewCaptions history search =
     let
-        isLetter char =
-            contains (Regex.regex "\\w") <| toString char
-
-        words line =
+        cleanStr line =
             line
                 |> String.toLower
-                |> String.filter isLetter
-                |> String.split " "
 
-        wordScore word =
-            match [ addPenalty 1, removePenalty 1 ] [] search word |> .score
+        lines =
+            history.current.captions
 
-        totalWordScore line =
-            words line
-                |> List.map wordScore
-                |> List.foldl (+) 0
+        fuzzyMatch line =
+            let
+                text =
+                    cleanStr line.text
+            in
+            ( match [] [ " " ] search text, line )
+
+        scoredLines =
+            List.map fuzzyMatch lines
+
+        sortedLines =
+            List.sortBy (\m -> Tuple.first m |> .score) scoredLines
     in
-    List.filter (\c -> totalWordScore c.text >= 2000) history.current.captions
+    if search == "" then
+        List.map viewOneCaption history.current.captions
+    else
+        List.map viewSearchedCaption sortedLines
 
 
-viewCaptions : List Line -> List (Html Msg)
-viewCaptions lines =
-    List.map
-        (\caption ->
-            p
-                [ onClick <| SkipToTime caption.time
-                , class "transcript__caption"
-                ]
-                [ text <| viewTime caption.time ++ ": " ++ caption.text ]
-        )
-        lines
+viewOneCaption : Line -> Html Msg
+viewOneCaption line =
+    p
+        [ onClick <| SkipToTime line.time
+        , class "transcript__caption"
+        ]
+        [ text <| viewTime line.time ++ ": " ++ line.text ]
+
+
+viewSearchedCaption : ( Fuzzy.Result, Line ) -> Html Msg
+viewSearchedCaption ( result, line ) =
+    let
+        isKey index =
+            List.foldl
+                (\e sum ->
+                    if not sum then
+                        List.member (index - e.offset) e.keys
+                    else
+                        sum
+                )
+                False
+                result.matches
+
+        isMatch index =
+            List.foldl
+                (\e sum ->
+                    if not sum then
+                        e.offset <= index && (e.offset + e.length) > index
+                    else
+                        sum
+                )
+                False
+                result.matches
+
+        color index =
+            if isKey index then
+                [ ( "color", "red" ) ]
+            else
+                []
+
+        bgColor index =
+            if isMatch index then
+                [ ( "background-color", "yellow" ) ]
+            else
+                []
+
+        hStyle index =
+            style (color index ++ bgColor index)
+
+        accumulateChar c ( sum, index ) =
+            ( sum ++ [ span [ hStyle index ] [ c |> String.fromChar |> text ] ], index + 1 )
+
+        highlight =
+            String.foldl accumulateChar ( [ span [] [ viewTime line.time ++ ": " |> text ] ], 0 ) line.text
+    in
+    p
+        [ onClick <| SkipToTime line.time
+        , class "transcript__caption"
+        ]
+        (Tuple.first highlight)
 
 
 viewTime : Float -> String
